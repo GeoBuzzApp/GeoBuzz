7e20cbe1d1be4ad54288ae6a1cc81ed2c159f973
GeoBuzz <GoeBuzzApp@hotmail.com>
2013-03-24 11:47:32 -0300

    Things

--- js/index.js
+++ js/index.js
@@ -6,3 +6,16 @@ var client = new WindowsAzure.MobileServiceClient(
 $(document).on("vclick", "#loginPageRegister", function(){
 	$.mobile.changePage("register.html");
 });
+
+
+$(document).on("vclick", "#loginPageLogin", function(){
+	
+	var user_table = client.getTable("user");
+	
+	user_table.where({email:$("#loginPageUserName").val().toLowerCase(), password:$("#loginPagePassword").val().toLowerCase()}).read().done(function(result){
+		
+		if(result.length != 1)
+			$("#loginPageMessage").html("Incorrect username and password");
+	});
+	
+});
